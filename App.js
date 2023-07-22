08001f0cbed502f59cc6980b4a834fc1d3b928c9
Coderner <shreyashukla680@gmail.com>
2023-07-22 11:49:29 +0530

    learnt React components and JSX

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 let obj="working fine";
 console.log(obj);
 
-let heading = React.createElement("h1",{id:"heading"},"Testing Heading");
+let Heading =()=> (<h1>Heading</h1>)
+
+
+let Para = () => {
+    const sayHi = () =>{
+        alert("It is working!!");
+    };
+    return(
+      <>
+        
+         <p>Hi campers</p>
+         <p>How is code going?</p>
+         <p>What is up?</p>
+         {<Heading/>}
+         {new Date().toDateString()}
+         <br/>
+         {10+8} is eigtheen
+         <button onClick={sayHi}>Click to alert</button>
+      </>
+    );
+}
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); 
+root.render(<Para/>); 

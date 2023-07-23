9764f7c6bfd5925af455f1a8e5818d552149be14
Coderner <shreyashukla680@gmail.com>
2023-07-23 16:04:50 +0530

    state variables and use state

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,45 @@
 import { RestrauntList } from "../Config";
 import RestrauntCard from "./RestrauntCard";
+import {useState} from "react";
+
+function filterData(searchText,RestrauntList){
+    const filterData= RestrauntList.filter((restraunt) => restraunt.info.name.includes(searchText));
+    return filterData;
+}
 
 const Body = () => {
+    const [restraunts, setRestraunts] = useState(RestrauntList); 
+    const [searchText, setSearchText] = useState("");
+    
     return(
-      <div class="restraunt-list">
-        {
-          RestrauntList.map((Restraunt) => {
-            return <RestrauntCard {...Restraunt.info} />
-          })
-        }
+        <>
+            <div className="search-container"> 
+                <input
+                  type="text"
+                  className="search-input"
+                  placeholder="Search"
+                  value={searchText}
+                  onChange={(e)=>{
+                    setSearchText(e.target.value)
+                  }}
+                />
+                <button className="search-btn"
+                  onClick={()=>{
+                    const data = filterData(searchText, RestrauntList);
+                    setRestraunts(data);
+                  }}
+                  >
+                    Search</button>
+               
+            </div>
+            <div className="restraunt-list">
+             {
+               restraunts.map((Restraunt) => {
+               return <RestrauntCard {...Restraunt.info} />
+               })
+             }
       </div>
+        </>
     );
   }
 

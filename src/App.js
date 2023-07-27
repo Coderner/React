92afe885e0ee10ece23bd690caaf5ced932c7629
Coderner <shreyashukla680@gmail.com>
2023-07-27 12:39:35 +0530

    Reading Errors using useRouteError

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
 
 let AppLayout = () => {
@@ -19,7 +20,8 @@ let AppLayout = () => {
 const appRouter = createBrowserRouter([
   {
     path:"/",
-    element: <AppLayout/>
+    element: <AppLayout/>,
+    errorElement: <Error/>
   },
   {
     path:"/about",
